61f23475f6653b6280f6d60a1504be38e60a2091
KatyaBahai <katyxa-two@yandex.ru>
2024-12-28 16:33:06 +0300

    changed genres

--- db/scripts/006_dml_insert_film_sessions.sql
+++ db/scripts/006_dml_insert_film_sessions.sql
@@ -1,5 +1,5 @@
-INSERT into film_sessions(film_id, hall_id, start_time, end_time, price) values(5, 1, '2025-11-19 15:30:00', '2025-11-19 17:30:00', 500);
-INSERT into film_sessions(film_id, hall_id, start_time, end_time, price) values(6, 2, '2025-11-19 20:30:00', '2025-11-19 22:30:00', 500);
-INSERT into film_sessions(film_id, hall_id, start_time, end_time, price) values(7, 1, '2025-11-20 15:30:00', '2025-11-19 17:30:00', 500);
-INSERT into film_sessions(film_id, hall_id, start_time, end_time, price) values(4, 2, '2025-11-20 20:30:00', '2025-11-19 22:30:00', 500);
-INSERT into film_sessions(film_id, hall_id, start_time, end_time, price) values(3, 1, '2025-11-21 15:30:00', '2025-11-19 17:30:00', 500);
+INSERT into film_sessions(film_id, hall_id, start_time, end_time, price) values(8, 1, '2025-11-19 15:30:00', '2025-11-19 17:30:00', 500);
+INSERT into film_sessions(film_id, hall_id, start_time, end_time, price) values(9, 2, '2025-11-19 20:30:00', '2025-11-19 22:30:00', 500);
+INSERT into film_sessions(film_id, hall_id, start_time, end_time, price) values(10, 1, '2025-11-20 15:30:00', '2025-11-19 17:30:00', 500);
+INSERT into film_sessions(film_id, hall_id, start_time, end_time, price) values(11, 2, '2025-11-20 20:30:00', '2025-11-19 22:30:00', 500);
+INSERT into film_sessions(film_id, hall_id, start_time, end_time, price) values(12, 1, '2025-11-21 15:30:00', '2025-11-19 17:30:00', 500);
